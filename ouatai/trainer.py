65c4ea25d8f6676accf5d354e7fefbfc3d711517
Alexandre Bernard <alexandre.bernard115@gmail.com>
2021-09-04 20:32:44 +0200

    new list word with space

--- ouatai/trainer.py
+++ ouatai/trainer.py
@@ -94,7 +94,7 @@ if __name__ == '__main__':
     BUCKET_NAME = "wagon-data-677-bernard"
     WORKING_FOLDERS = ['npz_repo','checkpoints','logs']
     ### MODEL LIST TO TRAIN ###
-    categorielist = ['sheep', 'snake', 'skull', 'bread', 'apple']
+    categorielist = ['crab', 'windmill', 'cat', 'dragon', 'soccer ball']
     ### SCRIPT ###
 
     #Start training loop over model list
