6c818879534dc65807b83386ca2b924797e3e609
Alexandre Bernard <alexandre.bernard115@gmail.com>
2021-09-05 20:11:54 +0200

    new list

--- ouatai/trainer.py
+++ ouatai/trainer.py
@@ -94,7 +94,7 @@ if __name__ == '__main__':
     BUCKET_NAME = "wagon-data-677-bernard"
     WORKING_FOLDERS = ['npz_repo','checkpoints','logs']
     ### MODEL LIST TO TRAIN ###
-    categorielist = ['square', 'dolphin', 'owl']
+    categorielist = ['teddy-bear', 'envelope', 'firetruck']
     ### SCRIPT ###
 
     #Start training loop over model list
